65ca1d5fce6dc6a5910cdbd908bdc84bb8ce8769
Aishwary Gupta <guptashark@gmail.com>
2017-02-22 01:10:53 -0500

    Basic regex digit matching working

--- main.cpp
+++ main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
-
+#include <regex>
+#include <string>
 using std::cout;
 using std::cin;
 using std::endl;
+using std::string;
 
 int main(int argc, char *argv[]) {
 
 	(void)argc;
 	(void)argv;
 
+	string input;
+	string pattern ("[[:digit:]]");
+
+	std::regex digit(pattern);
+
+	while(true) {
+
+		cin >> input;
+		if(!cin) return 0;
+		if(input == "q") return 0;
+		if(std::regex_match(input, digit)) {
+
+			cout << "Matched" << endl;
+		} else {
+			cout << "Unmatched" << endl;
+		}
+	}
+
+
+
 	return 0;
 }
+
+
+
+
+

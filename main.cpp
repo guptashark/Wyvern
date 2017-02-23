7651e7257f93713cb6d39733ab14ac1d4ed770aa
Aishwary Gupta <guptashark@gmail.com>
2017-02-23 10:03:45 -0500

    Successful partial tokenization of input.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 #include <regex>
 #include <string>
+#include <vector>
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::vector;
 
-
-
-int main(int argc, char *argv[]) {
-
-	(void)argc;
-	(void)argv;
-
+void single_pattern_loop(string pattern) {
 	string input;
-	string pattern ("[[:digit:]]");
-	string identifier ("[_[:alnum:]]+");
-
-	std::regex reg(identifier);
+	std::regex reg(pattern);
 
 	while(true) {
-
 		cin >> input;
-		if(!cin) return 0;
-		if(input == "q") return 0;
+		if(!cin) return;
+		if(input == "q") return;
+
 		if(std::regex_match(input, reg)) {
 
 			cout << "Matched" << endl;
@@ -31,9 +24,75 @@ int main(int argc, char *argv[]) {
 			cout << "Unmatched" << endl;
 		}
 	}
+}
 
+/* TODO return a vector, or have one as a param? */
+void tokenize_input(vector<string> &sv, string input, string rgx_pattern) {
+	
+	std::regex reg(rgx_pattern);
+	string input_suffix = input;
+	string input_prefix;
+	while(!input_suffix.empty() && input_prefix.empty()) {
+		std::smatch sm;
+		std::regex_search(input_suffix, sm, reg);
+		input_prefix = sm.prefix();
+		if(input_prefix.empty()) {
+			sv.push_back(sm[0]);
+		}
+		input_suffix = sm.suffix();
+	}
+}
 
+void print_str_vector(vector<string> &sv) {
+	std::vector<string>::iterator it;
 
+	for(it = sv.begin(); it != sv.end(); it++) {
+		cout << "[" << *it << "] ";
+	}
+
+	cout << endl;
+}
+
+/* What we will try: 
+ * 1.) get the entire string in a line. 
+ * 2.) Run regex that matches all possible tokens. 
+ * 3.) If it works, split string, push on a vector, keep going. 
+ * 4.) Run the regex of each possible thing against the lexeme until it matches. 
+ */
+int main(int argc, char *argv[]) {
+
+	(void)argc;
+	(void)argv;
+
+	string input;
+	string digit ("[[:digit:]]");
+	string identifier ("[_[:alnum:]]+");
+	string test_pattern("abc|def");
+	string test_pattern_2("([_[:alnum:]]+)|(=)|([[:space:]]+)");
+
+	std::regex reg(test_pattern_2);
+/*
+	while(true) {
+		cout << "Enter a string to be matched: ";
+		getline(cin, input);
+		std::smatch sm;
+		std::regex_search(input, sm, reg);	
+
+		std::smatch::iterator it;
+		std::cout << "prefix=[" << sm.prefix() << "], ";
+		for(it = sm.begin(); it != sm.end(); it++) {
+			cout << "[" << *it << "], ";
+		}
+		std::cout << "suffix=[" << sm.suffix() << "]\n";
+	}	
+*/
+	while(true) {
+		getline(cin, input);
+		vector<string> sv;
+		tokenize_input(sv, input, test_pattern_2);
+		print_str_vector(sv);
+	}
+	
 	return 0;
 }
 
